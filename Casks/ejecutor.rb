ea934229e908595141c3b296850cf1e85aa9a6fd
mmag <ryazanovmaxim@gmail.com>
2026-04-10 17:35:44 +0300

    Add ejecutor cask

---
+++ Casks/ejecutor.rb
@@ -0,0 +1,11 @@
+cask "ejecutor" do
+  version "1.0"
+  sha256 "44546893135247861f2a159e1c23b5af068a709e973532ab0c865109677cac02"
+
+  url "https://github.com/mmag/ejecutor/releases/download/release/Ejecutor.app.zip"
+  name "Ejecutor"
+  desc "macOS menu bar app for cleaning and ejecting external drives"
+  homepage "https://github.com/mmag/ejecutor"
+
+  app "Ejecutor.app"
+end
