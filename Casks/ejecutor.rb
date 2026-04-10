b16880b8144a52950b7c5d8d580c83230b84ef81
mmag <ryazanovmaxim@gmail.com>
2026-04-10 17:45:18 +0300

    Use versioned GitHub release for ejecutor cask

--- Casks/ejecutor.rb
+++ Casks/ejecutor.rb
@@ -2,7 +2,7 @@ cask "ejecutor" do
   version "1.0"
   sha256 "44546893135247861f2a159e1c23b5af068a709e973532ab0c865109677cac02"
 
-  url "https://github.com/mmag/ejecutor/releases/download/release/Ejecutor.app.zip"
+  url "https://github.com/mmag/ejecutor/releases/download/v1.0/Ejecutor.app.zip"
   name "Ejecutor"
   desc "macOS menu bar app for cleaning and ejecting external drives"
   homepage "https://github.com/mmag/ejecutor"
