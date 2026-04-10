5ba8be8001eb4fd663960cb21bba7d4ca13ea5a9
mmag <ryazanovmaxim@gmail.com>
2026-04-10 17:54:23 +0300

    Fix cask description

--- Casks/ejecutor.rb
+++ Casks/ejecutor.rb
@@ -2,9 +2,9 @@ cask "ejecutor" do
   version "1.0"
   sha256 "44546893135247861f2a159e1c23b5af068a709e973532ab0c865109677cac02"
 
-  url "https://github.com/mmag/ejecutor/releases/download/v1.0/Ejecutor.app.zip"
+  url "https://github.com/mmag/ejecutor/releases/download/v#{version}/Ejecutor.app.zip"
   name "Ejecutor"
-  desc "macOS menu bar app for cleaning and ejecting external drives"
+  desc "Menu bar app for cleaning and ejecting external drives"
   homepage "https://github.com/mmag/ejecutor"
 
   app "Ejecutor.app"
