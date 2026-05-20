a9313f3a13eeb6bf12d51408a8fe461679a19291
mmag <ryazanovmaxim@gmail.com>
2026-05-21 01:10:01 +0300

    tikatype 1.0.2

--- Casks/tikatype.rb
+++ Casks/tikatype.rb
@@ -1,6 +1,6 @@
 cask "tikatype" do
-  version "1.0.1"
-  sha256 "a4bd5c9b715a84c1754cba3ccc90e4b3d1cb0fe23dd78bbaeb30f09c2ac40888"
+  version "1.0.2"
+  sha256 "0629bfab4f0f17eba7800216f55f6b8682b35f207e9dd2269b407b6d29d6f12b"
 
   url "https://github.com/mmag/Tikatype/releases/download/#{version}/Tikatype-#{version}-app.zip"
   name "Tikatype"
