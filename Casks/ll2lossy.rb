ca5d984067008851e89565fad842f09c25a7da6a
mmag <ryazanovmaxim@gmail.com>
2026-05-28 01:51:37 +0300

    Update ll2lossy to v0.1.1, fix cask syntax

--- Casks/ll2lossy.rb
+++ Casks/ll2lossy.rb
@@ -1,13 +1,13 @@
 cask "ll2lossy" do
-  version "0.1"
-  sha256 "574fc3f83cd4c6a0c4294c498edb4b9d725c4853f9d4c0fcedab9429fde9d490"
+  version "0.1.1"
+  sha256 "d8fbbd204b55ac0ef2941f11574fccf630f0fb7a5ecab448792d45a7a5877909"
 
   url "https://github.com/mmag/ll2lossy/releases/download/v#{version}/LosslessToMP3.app.zip"
   name "LosslessToMP3"
   desc "Batch converter of lossless audio to MP3"
   homepage "https://github.com/mmag/ll2lossy"
 
-  depends_on :macos => ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "LosslessToMP3.app"
 end
