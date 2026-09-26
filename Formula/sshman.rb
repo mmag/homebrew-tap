1614ef349548fc9dd4f2406ac485ae794e0adb34
mmag <ryazanovmaxim@gmail.com>
2026-09-26 15:08:33 +0300

    sshman 0.2.0

--- Formula/sshman.rb
+++ Formula/sshman.rb
@@ -5,23 +5,23 @@ class Sshman < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/mmag/sshmanager/releases/download/v0.1.0/sshman-darwin-arm64"
-      sha256 "67397411d4d289972093a4ab4f37ea856bf4b47f327757f3b0a4f6428486ef92"
+      url "https://github.com/mmag/sshmanager/releases/download/v0.2.0/sshman-darwin-arm64"
+      sha256 "b7a232dcf00845217da70cf9f5de155c8ee1ed8d28324d2894f4c441748ff778"
     end
     on_intel do
-      url "https://github.com/mmag/sshmanager/releases/download/v0.1.0/sshman-darwin-amd64"
-      sha256 "7d760eb39d5c926692dd91c991dddddf32f1341dab2cd6093d847eff76558c33"
+      url "https://github.com/mmag/sshmanager/releases/download/v0.2.0/sshman-darwin-amd64"
+      sha256 "00762f002f193ecfa13baea6adec45989966b038605be0f0c17e624b2e1b871b"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/mmag/sshmanager/releases/download/v0.1.0/sshman-linux-arm64"
-      sha256 "568f5a5840641a44df63a10d855d93d8ab94244b0f1e56480dd9c08de4d410de"
+      url "https://github.com/mmag/sshmanager/releases/download/v0.2.0/sshman-linux-arm64"
+      sha256 "99b347a75cce813dab6db617b8ce4ea8221654d7b8fe4bd853e14e7570869aba"
     end
     on_intel do
-      url "https://github.com/mmag/sshmanager/releases/download/v0.1.0/sshman-linux-amd64"
-      sha256 "1d2c9d42ed9b9491bd739aaef9a0ed5c7f93797b2309246f421eb555f5b86e4a"
+      url "https://github.com/mmag/sshmanager/releases/download/v0.2.0/sshman-linux-amd64"
+      sha256 "b8b92647e9ea4590163374d8d0b48b1415b16247a337bdc443c58b8081c403a1"
     end
   end
 
